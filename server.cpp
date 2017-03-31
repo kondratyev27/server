68546129e0b6da4a363c06a537a5cc31aef74f8e
kondratyev <123427@mail.ru>
2017-04-01 00:11:42 +0300

    optimize my processing/sorting algorithm

--- server.cpp
+++ server.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <string.h>
+#include <vector>
 
 using namespace  std;
 
@@ -21,24 +22,51 @@ void Server::setError(const string &errMesg){
 }
 
 void Server::processingMsg(const string& msg){
-    char processedMsg [msg.length()+1];
-    memset(processedMsg, 0, msg.size()+1);
-    remove_copy_if(msg.begin(), msg.end(), processedMsg , [](char c){
-        return !isdigit(c);
-    });
-
-    string processedMsgStr(processedMsg);
-    sort(processedMsgStr.begin(), processedMsgStr.end(), greater<char>());
-    if(!processedMsgStr.empty()){
-        cout << "Desc sorted msg only with numbers: " << processedMsgStr << endl;
-        cout << "Max value: " << processedMsgStr.at(0) << endl;
-        cout << "Min value: " << processedMsgStr.at(processedMsgStr.length()-1) << endl;
-        int sum = 0;
-        for( auto c :processedMsgStr  ){
-            sum += stoi(&c);
+    vector <string> digits;
+    digits.resize(10);
+    int sum = 0;
+    bool hasDig = 0;
+    for( auto c : msg  ){
+       if(isdigit(c)){
+           int oneDigit = stoi(&c);
+           digits[oneDigit] += c;
+           sum += oneDigit;
+           hasDig = true;
+       }
+    }
+    if(hasDig){
+        string sortedDigits;
+        cout << "Desc sorted msg only with numbers: ";
+        for(auto it = digits.rbegin(); it != digits.rend(); it++){
+            cout << *it;
+            sortedDigits += *it;
         }
+        cout << endl;
+        cout << "Max value: " << sortedDigits.at(0) << endl;
+        cout << "Min value: " << sortedDigits.at(sortedDigits.length()-1) << endl;
         cout << "Sum of numbers: " << sum << endl << endl;
     }else{
         cout << "There are no numbers in message. \n";
     }
+
+//    char processedMsg [msg.length()+1];
+//    memset(processedMsg, 0, msg.size()+1);
+//    remove_copy_if(msg.begin(), msg.end(), processedMsg , [](char c){
+//        return !isdigit(c);
+//    });
+
+//    string processedMsgStr(processedMsg);
+//    sort(processedMsgStr.begin(), processedMsgStr.end(), greater<char>());
+//    if(!processedMsgStr.empty()){
+//        cout << "Desc sorted msg only with numbers: " << processedMsgStr << endl;
+//        cout << "Max value: " << processedMsgStr.at(0) << endl;
+//        cout << "Min value: " << processedMsgStr.at(processedMsgStr.length()-1) << endl;
+//        int sum = 0;
+//        for( auto c :processedMsgStr  ){
+//            sum += stoi(&c);
+//        }
+//        cout << "Sum of numbers: " << sum << endl << endl;
+//    }else{
+//        cout << "There are no numbers in message. \n";
+//    }
 }
